755805822fa60cb917542a35a1392226ed77a5d3
Rohit3129 <98886742+Rohit3129@users.noreply.github.com>
2026-02-19 02:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (71.47%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,26 +1,13 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         
-        def Nxtvalchar(str, index):
-            backspace = 0
-            while index >=0:
-                if backspace == 0 and str[index] != "#":
-                    break
-                elif str[index] == "#":
-                    backspace += 1
+        def nxt(string):
+            stack = []
+            for ch in string:
+                if ch == "#":
+                    if stack:
+                        stack.pop()
                 else:
-                    backspace -= 1
-                index -= 1
-            return index
-        Is = len(s) - 1
-        It = len(t) - 1
-        while Is >=0 or It >=0:
-            Is = Nxtvalchar(s, Is)
-            It = Nxtvalchar(t, It)
-            ch_s = s[Is] if Is >= 0 else ""
-            ch_t = t[It] if It >= 0 else ""
-            if ch_s != ch_t:
-                return False
-            Is -= 1
-            It -= 1
-        return True
+                    stack.append(ch)
+            return "".join(stack)
+        return nxt(s) == nxt(t)
