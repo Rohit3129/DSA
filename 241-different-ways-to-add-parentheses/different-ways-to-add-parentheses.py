173389d63626929cbb3a77626a784048d327beb9
Rohit3129 <98886742+Rohit3129@users.noreply.github.com>
2026-02-26 12:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.24%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -5,7 +5,10 @@ class Solution:
             "-" :lambda x, y : x - y,
             "*" :lambda x, y : x * y,
         }
+        memo = {}
         def backtrack(left, right):
+            if (left, right) in memo:
+                return memo[(left, right)]
             res = []
             for i in range(left, right+1):
                 op = expression[i]
@@ -17,5 +20,6 @@ class Solution:
                             res.append(operations[op](n1, n2))
             if res == []:
                 res.append(int(expression[left:right+1]))
+            memo[(left, right)] = res
             return res
         return backtrack(0, len(expression)-1)
