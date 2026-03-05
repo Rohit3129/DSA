f00752493cdfe6c0bc800567e45b20ff2d3349ad
Rohit3129 <98886742+Rohit3129@users.noreply.github.com>
2026-03-05 18:57:46 +0530

    Time: 288 ms (29.38%) | Memory: 20.2 MB (90.87%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,23 +1,21 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        dist = [[float('inf')]*n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+        for u, v, w in edges:
+            dist[u][v] = w
+            dist[v][u] = w
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        
         result_city = -1
         min_reach = float('inf')
-        for src in range(n):
-            dist = [float('inf')]*n
-            dist[src] = 0
-            for _ in range(n -1): 
-                for u, v, w in edges:
-                    if dist[u] + w < dist[v]:
-                        dist[v] = dist[u] + w
-                    if dist[v] + w < dist[u]:
-                        dist[u] = dist[v] + w
-            count = 0
-            for i in range(n):
-                if i != src and dist[i] <= distanceThreshold:
-                    count += 1
-            if count <= min_reach:
+        for i in range(n):
+            count = sum(1 for j in range(n) if i != j and dist[i][j] <= distanceThreshold)
+            if count<= min_reach:
                 min_reach = count
-                result_city = src
+                result_city = i
         return result_city
-
-
